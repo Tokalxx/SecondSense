31bd7fed9522a364289b68cf6d9ef6540313f9d1
Siyabonga Zunguza <69678930+Bonga50@users.noreply.github.com>
2023-05-16 20:21:20 +0200

    Made Tablayout work
    Made the tab layout work and started with the recent section

--- reSecondSense_V01/app/src/main/java/com/example/resecondsense_v01/MainActivity.kt
+++ reSecondSense_V01/app/src/main/java/com/example/resecondsense_v01/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.resecondsense_v01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import com.example.resecondsense_v01.databinding.ActivityHomePageBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomePageBinding
+
+    val data = listOf(
+        RecentEntry(R.drawable.the_goat, "Title 1", "Date 1"),
+        RecentEntry(R.drawable.the_cow, "Title 2", "Date 2"),
+        RecentEntry(R.drawable.the_other_goat, "Title 3", "Date 3"),
+        // Add more items as needed
+    )
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup_page)
+        setContentView(R.layout.activity_home_page)
+
+
+        // code fo creating the Tablayout
+        // video for this https://youtu.be/DJiqBhqyZeg
+        var tabLayout: TabLayout = findViewById(R.id.tabNavigation)
+        var viewPage : ViewPager = findViewById(R.id.vpNavigation)
+        // Create an instance of your custom adapter
+        val vpAdapter = VPAdapter(supportFragmentManager)
+        // Add more fragments as needed
+        vpAdapter.addFragment(Home(),"Home")
+        // Set the adapter to the ViewPager
+        viewPage.adapter = vpAdapter
+        tabLayout.setupWithViewPager(viewPage)
+
+        //Viewbinding
+
+
     }
 }
---
+++ reSecondSense_V01/app/src/main/java/com/example/resecondsense_v01/VPAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.resecondsense_v01
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class VPAdapter(fragmentActivity: FragmentManager) : FragmentStatePagerAdapter(fragmentActivity,
+    BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    private var fragmentList: MutableList<Fragment> = ArrayList()
+    private var titleList: MutableList<String> = ArrayList()
+    override fun getCount(): Int {
+       return fragmentList.size
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return fragmentList[position]
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return titleList[position]
+    }
+
+    fun addFragment(fragment: Fragment, title: String) {
+        fragmentList.add(fragment)
+        titleList.add(title)
+    }
+
+
+}
