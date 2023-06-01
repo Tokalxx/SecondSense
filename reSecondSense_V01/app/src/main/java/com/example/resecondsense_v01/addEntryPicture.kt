f9794c7af69af2a39c36b01bf923ca638bf078e6
Siyabonga Zunguza <69678930+Bonga50@users.noreply.github.com>
2023-06-01 21:36:50 +0200

    Added entry picture layout

---
+++ reSecondSense_V01/app/src/main/java/com/example/resecondsense_v01/addEntryPicture.kt
@@ -0,0 +1,11 @@
+package com.example.resecondsense_v01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class addEntryPicture : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_entry_picture)
+    }
+}
