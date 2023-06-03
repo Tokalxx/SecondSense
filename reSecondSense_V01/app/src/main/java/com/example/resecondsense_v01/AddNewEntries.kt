3f14921ca046a1d31e08829acf559ade4f0136ce
Siyabonga Zunguza <69678930+Bonga50@users.noreply.github.com>
2023-06-03 12:55:32 +0200

    made a drop down menu for catgories in the create category screen

--- reSecondSense_V01/app/src/main/java/com/example/resecondsense_v01/AddNewEntries.kt
+++ reSecondSense_V01/app/src/main/java/com/example/resecondsense_v01/AddNewEntries.kt
@@ -3,13 +3,29 @@ package com.example.resecondsense_v01
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.Toast
 
 class AddNewEntries : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new_entries)
 
+
+        //Filling the category drop down
+        val items = listOf("DummyData","Math","English","Afrikaans")
+        val autoComplete : AutoCompleteTextView = findViewById(R.id.cmbCategory)
+
+        val adapter = ArrayAdapter(this,R.layout.template_categorydropdown,items)
+        autoComplete.setAdapter(adapter)
+
+        autoComplete.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
+            val selectedItem =  adapterView.getItemAtPosition(i)
+            Toast.makeText(this, "Item ${selectedItem}", Toast.LENGTH_SHORT).show()
+        }
         //back button
         val backbutton: Button = findViewById(R.id.btnBackHome)
         backbutton.setOnClickListener {
--- reSecondSense_V01/app/src/main/java/com/example/resecondsense_v01/addEntryPicture.kt
+++ reSecondSense_V01/app/src/main/java/com/example/resecondsense_v01/addEntryPicture.kt
@@ -2,12 +2,17 @@ package com.example.resecondsense_v01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.Toast
 
 class addEntryPicture : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_entry_picture)
+
 
         //back button
         val backbutton: Button = findViewById(R.id.btnBackHome)
