649781dc34a985f010d99388587fe27e404b3454
ST10091114 <69678930+Bonga50@users.noreply.github.com>
2023-08-04 10:54:08 +0200

    Update AddNewCategories.kt

--- reSecondSense_V01/app/src/main/java/com/example/resecondsense_v01/AddNewCategories.kt
+++ reSecondSense_V01/app/src/main/java/com/example/resecondsense_v01/AddNewCategories.kt
@@ -59,3 +59,16 @@ class AddNewCategories : AppCompatActivity() {
         }
     }
 }
+
+/*public class NetworkUtil {
+    private static String WEATHERBASE_URL =
+            "https://dataservice.accuweather.com/forecasts/v1/daily/5day/305605";
+    private static String PARAM_METRIC = "metric";
+    private static String METRIC_VALUE = "true";
+    private static String PARAM_API_KEY = "apikey";
+    private static String LOGGING_TAG = "URLWECREATED";
+
+    public NetworkUtil() {
+    }
+}
+*/
